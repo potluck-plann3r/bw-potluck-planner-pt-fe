833fee96b75997b6feb1315995052927423c4265
Pherpher089 <Christutor089@gmail.com>
2019-07-11 05:41:31 -0700

    Started styling the login view with styled-components. Still needs work

--- potluck-planner/src/components/Login/Login.js
+++ potluck-planner/src/components/Login/Login.js
@@ -1,7 +1,44 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {login} from '../../actions'
+import Styled from 'styled-components'
 
+const Container = Styled.div`
+    width: 100%;
+    
+`
+
+const HeaderDiv = Styled.div`
+    height: 6rem;
+    background-color: green;
+`
+const LoginCard = Styled.div`
+    width: 30rem;
+    height: 40rem;
+    background-color: red;
+    margin-top: 10rem;
+    display: flex;
+
+`
+const LoginForm = Styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    align-content: center;
+`
+const ContentDiv = Styled.div`
+    height: 0 auto;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+
+`
+const LogInContent = Styled.div`
+    width: 50rem;
+    background-color: blue;
+`
 
 class Login extends React.Component{
     constructor(props){
@@ -24,23 +61,33 @@ class Login extends React.Component{
     
     render(){
         return(
-            <div>
-                <form onSubmit={this.onSubmit}>
-                    <input placeholder='User Name'
-                        type='text'
-                        name='username'
-                        value={this.state.username}
-                        onChange={this.changeHadeler}>
-                    </input>
-                    <input placeholder='Password'
-                        type='text'
-                        name='password'
-                        value={this.state.password}
-                        onChange={this.changeHadeler}>
-                    </input>
-                    <button>Log in</button>
-                </form>
-            </div>
+            <Container>
+                <HeaderDiv>
+
+                </HeaderDiv>
+                <ContentDiv>
+                    <LogInContent>
+
+                    </LogInContent>
+                    <LoginCard>
+                        <LoginForm onSubmit={this.onSubmit}>
+                            <input placeholder='User Name'
+                                type='text'
+                                name='username'
+                                value={this.state.username}
+                                onChange={this.changeHadeler}>
+                            </input>
+                            <input placeholder='Password'
+                                type='text'
+                                name='password'
+                                value={this.state.password}
+                                onChange={this.changeHadeler}>
+                            </input>
+                            <button>Log in</button>
+                        </LoginForm>
+                    </LoginCard>
+                </ContentDiv>
+            </Container>
         );
     }
 }
--- potluck-planner/src/reducers/LoginReducer.js
+++ potluck-planner/src/reducers/LoginReducer.js
@@ -1,12 +1,16 @@
 import {LOGIN_START} from '../actions'
 
 const initalState = {
-    testState: ''
+    isLoggingIn: false
 }
 
  export const reducer = (state = initalState, action) =>{
      switch(action.type){
         case LOGIN_START: 
+            return{
+                ...state,
+                isLoggingIn: true
+            }
         break;
 
         default:
