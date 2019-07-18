1d8575df1a06ac00f79c5fb5132e3b88e9e69eee
Robert Gant <demontegant@gmail.com>
2019-07-17 20:40:30 -0500

    testing just to add me on git branch

--- potluck-planner/src/App.js
+++ potluck-planner/src/App.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
-import Login from './components/Login/Login'
-import Registration from './components/Login/Registration'
-import UserDashboard from './components/DashBoard/UserDashboard'
-import PrivateRoute from './components/Login/PrivateRoute'
-const WithAuth = PrivateRoute(UserDashboard)
-
-class App extends React.Component{
-  constructor(props){
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Registration from './components/Login/Registration';
+import UserDashboard from './components/DashBoard/UserDashboard';
+import PrivateRoute from './components/Login/PrivateRoute';
+const WithAuth = PrivateRoute(UserDashboard);
+//random change lol
+class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
-  render(){
+  render() {
     return (
       <Router>
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Registration}  />
-        <Route exact to='/protected' render={props => <WithAuth {...props}/>}/>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Registration} />
+        <Route
+          exact
+          to="/protected"
+          render={props => <WithAuth {...props} />}
+        />
       </Router>
     );
   }
-  
 }
 
 export default App;
