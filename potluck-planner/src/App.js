3618bc817f100ed278f820bf05485921c6af6d10
Robert Gant <demontegant@gmail.com>
2019-07-18 23:05:42 -0500

    finished merging code had to fix some issues but they are now resolved

--- potluck-planner/src/App.js
+++ potluck-planner/src/App.js
@@ -4,19 +4,81 @@ import Login from './components/Login/Login';
 import Registration from './components/Login/Registration';
 import UserDashboard from './components/DashBoard/UserDashboard';
 import PrivateRoute from './components/Login/PrivateRoute';
+import NavBar from './components/NavBar';
+import './App.scss';
 const WithAuth = PrivateRoute(UserDashboard);
-//random change lol
+
 class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  // }
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isLogginActive: false
+    };
+  }
+
+  componentDidMount() {
+    //Add .right by default
+    this.rightSide.classList.add('right');
+  }
+
+  changeState() {
+    const { isLogginActive } = this.state;
+
+    if (isLogginActive) {
+      this.rightSide.classList.remove('right');
+      this.rightSide.classList.add('left');
+    } else {
+      this.rightSide.classList.remove('left');
+      this.rightSide.classList.add('right');
+    }
+    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
   }
 
   render() {
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? 'Register' : 'Login';
+    const currentActive = isLogginActive ? 'Login' : 'Register';
     return (
+      // <Router>
+      //   <NavBar />
+      //   <Route path="/login" component={Login} />
+      //   <Route path="/register" component={Registration} />
+      //   <Route
+      //     exact
+      //     to="/protected"
+      //     render={props => <WithAuth {...props} />}
+      //   />
+      // </Router>
+
       <Router>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Registration} />
+        <NavBar
+          current={current}
+          currentActive={currentActive}
+          containerRef={ref => (this.rightSide = ref)}
+          onClick={this.changeState.bind(this)}
+        />
+        <div className="App">
+          <div className="login">
+            <div className="container" ref={ref => (this.container = ref)}>
+              {isLogginActive &&
+                (<Route path="/login" component={Login} />,
+                <Login containerRef={ref => (this.current = ref)} />)}
+              {!isLogginActive &&
+                (<Route path="/register" component={Registration} />,
+                <Registration containerRef={ref => (this.current = ref)} />)}
+            </div>
+            <RightSide
+              current={current}
+              currentActive={currentActive}
+              containerRef={ref => (this.rightSide = ref)}
+              onClick={this.changeState.bind(this)}
+            />
+          </div>
+        </div>
         <Route
           exact
           to="/protected"
@@ -26,5 +88,18 @@ class App extends React.Component {
     );
   }
 }
+const RightSide = props => {
+  return (
+    <div
+      className="right-side"
+      ref={props.containerRef}
+      onClick={props.onClick}
+    >
+      <div className="inner-container">
+        <div className="text">{props.current}</div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
