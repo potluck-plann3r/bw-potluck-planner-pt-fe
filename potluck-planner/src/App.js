0259931baabe14e3cc4a4c1eaeb68b7f83e904d7
Robert Gant <demontegant@gmail.com>
2019-07-19 21:11:38 -0500

    pushing

--- potluck-planner/src/App.js
+++ potluck-planner/src/App.js
@@ -1,84 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Registration from './components/Login/Registration';
 import UserDashboard from './components/DashBoard/UserDashboard';
 import PrivateRoute from './components/Login/PrivateRoute';
 import NavBar from './components/NavBar';
 import './App.scss';
+import FrontPage from './components/FrontPage';
 const WithAuth = PrivateRoute(UserDashboard);
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {};
-  // }
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLogginActive: false
-    };
-  }
-
-  componentDidMount() {
-    //Add .right by default
-    this.rightSide.classList.add('right');
-  }
-
-  changeState() {
-    const { isLogginActive } = this.state;
-
-    if (isLogginActive) {
-      this.rightSide.classList.remove('right');
-      this.rightSide.classList.add('left');
-    } else {
-      this.rightSide.classList.remove('left');
-      this.rightSide.classList.add('right');
-    }
-    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
-  }
-
   render() {
-    const { isLogginActive } = this.state;
-    const current = isLogginActive ? 'Register' : 'Login';
-    const currentActive = isLogginActive ? 'Login' : 'Register';
     return (
-      // <Router>
-      //   <NavBar />
-      //   <Route path="/login" component={Login} />
-      //   <Route path="/register" component={Registration} />
-      //   <Route
-      //     exact
-      //     to="/protected"
-      //     render={props => <WithAuth {...props} />}
-      //   />
-      // </Router>
-
       <Router>
-        <NavBar
-          current={current}
-          currentActive={currentActive}
-          containerRef={ref => (this.rightSide = ref)}
-          onClick={this.changeState.bind(this)}
-        />
-        <div className="App">
-          <div className="login">
-            <div className="container" ref={ref => (this.container = ref)}>
-              {isLogginActive &&
-                (<Route path="/login" component={Login} />,
-                <Login containerRef={ref => (this.current = ref)} />)}
-              {!isLogginActive &&
-                (<Route path="/register" component={Registration} />,
-                <Registration containerRef={ref => (this.current = ref)} />)}
-            </div>
-            <RightSide
-              current={current}
-              currentActive={currentActive}
-              containerRef={ref => (this.rightSide = ref)}
-              onClick={this.changeState.bind(this)}
-            />
-          </div>
-        </div>
+        <Route path="/home" component={FrontPage} />
+        <Route path="/login" component={FrontPage} />
+        <Route path="/protected" component={FrontPage} />
         <Route
           exact
           to="/protected"
@@ -88,16 +23,11 @@ class App extends React.Component {
     );
   }
 }
-const RightSide = props => {
+
+const test = () => {
   return (
-    <div
-      className="right-side"
-      ref={props.containerRef}
-      onClick={props.onClick}
-    >
-      <div className="inner-container">
-        <div className="text">{props.current}</div>
-      </div>
+    <div>
+      <h1>Not AUTH</h1>
     </div>
   );
 };
--- potluck-planner/src/components/Login/PrivateRoute.js
+++ potluck-planner/src/components/Login/PrivateRoute.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import {Route, Redirect} from 'react-router-dom'
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = Component =>{
-    return class extends React.Component{
-        render(){
-            if(localStorage.getItem('token')){
-                return <Component {...this.props}/>
-            } else {
-                console.log('redirect to Login')
-                return <Redirect to='/login' />
-            }
-        }
+const PrivateRoute = Component => {
+  return class extends React.Component {
+    render() {
+      if (localStorage.getItem('token')) {
+        return <Component {...this.props} />;
+      } else {
+        console.log('redirect to Login');
+        // return <Redirect to='/login' />
+        return <Redirect to="protected" />;
+      }
     }
-}
+  };
+};
 
 export default PrivateRoute;
