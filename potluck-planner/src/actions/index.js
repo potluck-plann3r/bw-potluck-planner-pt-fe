c459b35d31df33a2819ea1d80749314183ed9629
Pherpher089 <Christutor089@gmail.com>
2020-03-25 23:54:47 -0700

    Moved package.json, node_modules .gitnore and readme into the root of the app dir. Changed the begining of the route URLs in actions to a string template containing a .env variable (DEV_URL)

--- potluck-planner/src/actions/index.js
+++ potluck-planner/src/actions/index.js
@@ -1,58 +1,59 @@
-import axios from 'axios';
-export const LOGIN_START = 'LOGIN_START';
-export const REGISTER = 'REGISTER';
-export const LOGOUT = 'LOGOUT';
-export const GETPOTLUCKS = 'GETPOTLUCKS';
-export const SUCCESS = 'SUCCESS';
-export const FAILURE = 'FAILURE';
-export const CREATE_POTLUCK = 'CREATE_POTLUCK';
+import axios from "axios";
+export const LOGIN_START = "LOGIN_START";
+export const REGISTER = "REGISTER";
+export const LOGOUT = "LOGOUT";
+export const GETPOTLUCKS = "GETPOTLUCKS";
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
+export const CREATE_POTLUCK = "CREATE_POTLUCK";
 
+const devURL = process.env.DEV_URL;
 export const login = creds => dispatch => {
-  dispatch({ type: LOGIN_START });
-  return axios
-    .post('https://potluck-plann3r.herokuapp.com/api/auth/login', creds)
-    .then(res => {
-      localStorage.setItem('token', res.data.authToken);
-      window.alert(res.data.message);
-    })
-    .catch(err => console.log(err));
+	dispatch({ type: LOGIN_START });
+	return axios
+		.post(`${devURL}/api/auth/login`, creds)
+		.then(res => {
+			localStorage.setItem("token", res.data.authToken);
+			window.alert(res.data.message);
+		})
+		.catch(err => console.log(err));
 };
 
 export const register = regObj => dispatch => {
-  dispatch({ type: REGISTER });
-  return axios
-    .post('https://potluck-plann3r.herokuapp.com/api/auth/register', regObj)
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+	dispatch({ type: REGISTER });
+	return axios
+		.post(`${devURL}/api/auth/register`, regObj)
+		.then(res => console.log(res))
+		.catch(err => console.log(err));
 };
 
 export const getPotlucks = () => dispatch => {
-  dispatch({ type: GETPOTLUCKS });
+	dispatch({ type: GETPOTLUCKS });
 
-  axios
-    .get('https://potluck-plann3r.herokuapp.com/api/potlucks', {
-      headers: { authorization: localStorage.getItem('token') }
-    })
-    .then(res => {
-      console.log(res);
-      dispatch({ type: SUCCESS, payload: res.data });
-    })
-    .catch(err => {
-      console.log('This is actualy the error' + err);
-      dispatch({ type: FAILURE, payload: err });
-    });
+	axios
+		.get(`${devURL}/api/potlucks`, {
+			headers: { authorization: localStorage.getItem("token") },
+		})
+		.then(res => {
+			console.log(res);
+			dispatch({ type: SUCCESS, payload: res.data });
+		})
+		.catch(err => {
+			console.log("This is actualy the error" + err);
+			dispatch({ type: FAILURE, payload: err });
+		});
 };
 
 export const addPotluck = newPotluck => dispatch => {
-  console.log(newPotluck, 'blah');
-  dispatch({ type: CREATE_POTLUCK });
-  axios
-    .post('https://potluck-plann3r.herokuapp.com/api/potlucks', newPotluck, {
-      headers: { authorization: localStorage.getItem('token') }
-    })
-    .then(res => {
-      console.log(res);
-      dispatch({ type: SUCCESS, payload: res.data });
-    })
-    .catch(err => console.log(err));
+	console.log(newPotluck, "blah");
+	dispatch({ type: CREATE_POTLUCK });
+	axios
+		.post(`${devURL}/api/potlucks`, newPotluck, {
+			headers: { authorization: localStorage.getItem("token") },
+		})
+		.then(res => {
+			console.log(res);
+			dispatch({ type: SUCCESS, payload: res.data });
+		})
+		.catch(err => console.log(err));
 };
