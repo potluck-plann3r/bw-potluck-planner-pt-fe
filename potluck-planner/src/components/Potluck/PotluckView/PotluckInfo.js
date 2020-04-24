babc33d9eae85c3e2a5b1edc2530f11985854efd
Pherpher089 <Christutor089@gmail.com>
2020-04-23 17:05:13 -0700

    Fixed up the basic delete potluck functionality

--- potluck-planner/src/components/Potluck/PotluckView/PotluckInfo.js
+++ potluck-planner/src/components/Potluck/PotluckView/PotluckInfo.js
@@ -1,7 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
+import { deletePotluck, getPotlucks } from "../../../actions/index.js";
+import "./PotluckView.scss";
+
 class PotluckInfo extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			admin: "non-admin",
+		};
+	}
+	componentDidMount() {
+		if (this.props.admin === 0) {
+			this.setState({ ...this.state, admin: "admin" });
+		}
+	}
+
+	onDeletePotluck = async (e) => {
+		await this.props.deletePotluck(this.props.currentPotluck.id);
+		await this.props.getPotlucks();
+		this.props.history.push("/protected");
+	};
+
 	render() {
 		return (
 			<>
@@ -18,6 +39,12 @@ class PotluckInfo extends React.Component {
 					<p>{this.props.currentPotluck.locationPostcode}</p>
 					<p>{this.props.currentPotluck.locationCountry}</p>
 				</address>
+				<button
+					className={this.state.admin}
+					onClick={this.onDeletePotluck}
+				>
+					Delete Potluck
+				</button>
 			</>
 		);
 	}
@@ -29,4 +56,6 @@ const mapStateToProps = (state) => ({
 
 const PotluckInfoWithRouter = withRouter(PotluckInfo);
 
-export default connect(mapStateToProps, {})(PotluckInfoWithRouter);
+export default connect(mapStateToProps, { deletePotluck, getPotlucks })(
+	PotluckInfoWithRouter
+);
